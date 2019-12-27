3a10ac6109f75b1ef7e7e1e4f23fa9ddf22cc0b2
suhu3515 <suhu3515@gmail.com>
2019-12-27 23:14:30 +0530

    DAY 2 - PART2 ======== -> Updated instructions page -> stored name and score using shared preferences.
    TODO
    ========
    -> Add Instructions
    -> Add Profile info
    -> Direct check in to instruction if profile is saved.

--- app/src/main/java/com/vhddev/codequiz/InstructionActivity.java
+++ app/src/main/java/com/vhddev/codequiz/InstructionActivity.java
@@ -2,13 +2,32 @@ package com.vhddev.codequiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class InstructionActivity extends AppCompatActivity {
+
+    String name,t_score;
+    int score;
+    TextView tv_name,tv_score;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_instruction);
+
+        SharedPreferences sharedPreferences = getSharedPreferences("UserInfo",MODE_PRIVATE);
+
+        tv_name = findViewById(R.id.top_name);
+        tv_score = findViewById(R.id.top_score);
+
+        name = sharedPreferences.getString("UserName",null);
+        score = sharedPreferences.getInt("score",0);
+        t_score = String.valueOf(score);
+
+        tv_name.setText(name);
+        tv_score.setText(t_score);
+
     }
 }
--- app/src/main/java/com/vhddev/codequiz/loginActivity.java
+++ app/src/main/java/com/vhddev/codequiz/loginActivity.java
@@ -3,6 +3,7 @@ package com.vhddev.codequiz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,8 +12,8 @@ import android.widget.Toast;
 
 public class loginActivity extends AppCompatActivity {
 
-    String name;
-    int score,age;
+    String name,age;
+    int score;
     EditText et_n,et_a;
     Button btn;
 
@@ -27,20 +28,22 @@ public class loginActivity extends AppCompatActivity {
         btn = findViewById(R.id.btn_save);
         score=0;
 
-        name = et_n.getText().toString();
-        //age = Integer.parseInt(et_a.getText().toString());
-
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                name = et_n.getText().toString();
+                age = et_a.getText().toString();
+
+                SharedPreferences sharedPreferences = getSharedPreferences("UserInfo",MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putString("UserName",name);
+                editor.putInt("score",score);
+                editor.apply();
+
                 Intent ins_intent = new Intent(loginActivity.this,InstructionActivity.class);
-                ins_intent.putExtra("p_name",name);
-                ins_intent.putExtra("p_score",score);
                 startActivity(ins_intent);
-                }
-            });
-        }
-
-
+            }
+        });
 
     }
+}
