3473e9f190e996a739094e796d4b37fd3c616660
suhu3515 <suhu3515@gmail.com>
2019-12-28 22:54:08 +0530

    DAY 3 PART 3 =============== -> Updated Instructions. -> Added play and player info buttons(added intent)
    TODO
    ========
    -> Add Profile info
    -> Add Sample Questions.

--- app/src/main/java/com/vhddev/codequiz/InstructionActivity.java
+++ app/src/main/java/com/vhddev/codequiz/InstructionActivity.java
@@ -2,8 +2,11 @@ package com.vhddev.codequiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class InstructionActivity extends AppCompatActivity {
@@ -11,6 +14,7 @@ public class InstructionActivity extends AppCompatActivity {
     String name,t_score;
     int score;
     TextView tv_name,tv_score;
+    Button btn_play,btn_info;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +25,8 @@ public class InstructionActivity extends AppCompatActivity {
 
         tv_name = findViewById(R.id.top_name);
         tv_score = findViewById(R.id.top_score);
+        btn_play = findViewById(R.id.btn_play);
+        btn_info = findViewById(R.id.btn_p_info);
 
         name = sharedPreferences.getString("UserName",null);
         score = sharedPreferences.getInt("score",0);
@@ -28,6 +34,22 @@ public class InstructionActivity extends AppCompatActivity {
 
         tv_name.setText(name);
         tv_score.setText(t_score);
+
+        btn_info.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent infoIntent = new Intent(InstructionActivity.this,PlayerInfoActivity.class);
+                startActivity(infoIntent);
+            }
+        });
+
+        btn_play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent quesIntent = new Intent(InstructionActivity.this,Ques1Activity.class);
+                startActivity(quesIntent);
+            }
+        });
 
     }
 }
---
+++ app/src/main/java/com/vhddev/codequiz/Ques1Activity.java
@@ -0,0 +1,14 @@
+package com.vhddev.codequiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Ques1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ques1);
+    }
+}
